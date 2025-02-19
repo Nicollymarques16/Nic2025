d16df7e1ba893c5477c33b9dce743d68185b6b64
Nicollymarques16 <159157738+Nicollymarques16@users.noreply.github.com>
2025-02-19 12:35:54 -0300

    18/02

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+   
+
+    let tamanhoAtualfonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+    tamanhoAtualfonte += 0.1;
+    document.body.style.fontSize = 
+    });
+});
